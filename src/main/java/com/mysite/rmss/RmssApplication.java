9e8c3a4cc91ecbf93b35875c96be7e2f476ce951
TipsyBoy <gudah13@naver.com>
2022-11-17 20:54:57 +0900

    Initial commit

---
+++ src/main/java/com/mysite/rmss/RmssApplication.java
@@ -0,0 +1,13 @@
+package com.mysite.rmss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmssApplication.class, args);
+	}
+
+}
