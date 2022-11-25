f0f2b1891c2ac892b0cc7d48041c6611c92989b6
TipsyBoy <gudah13@naver.com>
2022-11-25 17:44:22 +0900

    @CurrentMember 확인2

--- src/main/java/com/mysite/rmss/controller/IndexController.java
+++ src/main/java/com/mysite/rmss/controller/IndexController.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
@@ -22,26 +23,13 @@ public class IndexController {
     private final MemberRepository memberRepository;
 
     @GetMapping("/")
-    public String home(Principal principal,
-                       @CurrentMember Member member) {
-        log.info("principal={}", principal);
-        if (principal != null) {
-            log.info("principal 을 받아서 사용하는 경우");
-            String name = principal.getName();
-            Member findMember = memberRepository.findByName(name)
-                    .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
-            log.info("findMember.getUsername()={}", findMember.getUsername());
-            log.info("findMember.getPassword()={}", findMember.getPassword());
-            log.info("findMember.getEmail()={}", findMember.getEmail());
+    public String home(@CurrentMember Member member, Model model) {
+        String welcome = "손";
+        if (member != null) {
+            welcome = member.getUsername();
         }
 
-        log.info("member={}", member);
-        if (member!=null) {
-            log.info("현재 인증된 사용자의 객체를 받아서 사용하는 경우");
-            log.info("member.getUsername()={}", member.getUsername());
-            log.info("member.getPassword()={}", member.getPassword());
-            log.info("member.getEmail()={}", member.getEmail());
-        }
+        model.addAttribute("welcome", welcome);
         return "home";
     }
 }
