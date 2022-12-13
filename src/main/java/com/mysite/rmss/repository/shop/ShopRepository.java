b9db12d96202fc4aba1a55bdee08c6da81d2298f
TipsyBoy <gudah13@naver.com>
2022-12-13 21:57:06 +0900

    ShopRepository 구현

---
+++ src/main/java/com/mysite/rmss/repository/shop/ShopRepository.java
@@ -0,0 +1,49 @@
+package com.mysite.rmss.repository.shop;
+
+import com.mysite.rmss.domain.shop.Shop;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class ShopRepository {
+
+    private final EntityManager em;
+
+    public Long save(Shop shop) {
+        em.persist(shop);
+        return shop.getId();
+    }
+
+    public Optional<Shop> findById(Long id) {
+        return Optional.ofNullable(em.find(Shop.class, id));
+    }
+
+    // Shop 이름으로 찾기
+    public Optional<Shop> findByTitle(String title) {
+        return em.createQuery("select s from Shop s where s.shopTitle = :title", Shop.class)
+                .setParameter("title", title)
+                .getResultList()
+                .stream().findAny();
+    }
+
+    public boolean existsByShopTitle(String shopTitle) {
+        List<Long> result = em.createQuery("select s.id from Shop s where s.shopTitle = :shopTitle", Long.class)
+                .setParameter("shopTitle", shopTitle)
+                .getResultList();
+
+        return result.size() > 0;
+    }
+
+    public boolean existsByUrl(String url) {
+        List<Long> result = em.createQuery("select s.id from Shop s where s.url = :url", Long.class)
+                .setParameter("url", url)
+                .getResultList();
+
+        return result.size() > 0;
+    }
+}
