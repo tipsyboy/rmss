0bf035ac8e819bd244e06639bca2b8edb8b71d65
TipsyBoy <gudah13@naver.com>
2022-12-21 18:25:21 +0900

    ItemRepository 작성

---
+++ src/main/java/com/mysite/rmss/repository/item/ItemRepository.java
@@ -0,0 +1,31 @@
+package com.mysite.rmss.repository.item;
+
+import com.mysite.rmss.domain.item.Item;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Repository
+public class ItemRepository {
+
+    private EntityManager em;
+
+    public Long save(Item item) {
+        em.persist(item);
+        return item.getId();
+    }
+
+    public Optional<Item> findById(Long id) {
+        return Optional.ofNullable(em.find(Item.class, id));
+    }
+
+    public Optional<Item> findByName(String itemName) {
+        return em.createQuery("select i from Item i where i.itemName = :itemName", Item.class)
+                .setParameter("itemName", itemName)
+                .getResultList()
+                .stream().findAny();
+    }
+}
