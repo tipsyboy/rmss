6c3d1123d8f768fc09a6dd032fd9ae32a9cd1226
TipsyBoy <gudah13@naver.com>
2023-01-16 16:54:25 +0900

    OrderRepository 작성

---
+++ src/main/java/com/mysite/rmss/repository/order/OrderRepository.java
@@ -0,0 +1,22 @@
+package com.mysite.rmss.repository.order;
+
+import com.mysite.rmss.domain.Order.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+
+@RequiredArgsConstructor
+@Repository
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+}
