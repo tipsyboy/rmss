52839a192a8fdf90573096a1dd4724ce4318a349
TipsyBoy <gudah13@naver.com>
2023-01-16 17:48:26 +0900

    OrderService 구현 준비

--- src/main/java/com/mysite/rmss/dto/order/OrderRequestDto.java
+++ src/main/java/com/mysite/rmss/dto/order/OrderRequestDto.java
@@ -8,4 +8,10 @@ public class OrderRequestDto {
     private Long itemId;
     private Long memberId;
     private Integer quantity;
+
+    public OrderRequestDto(Long itemId, Long memberId, Integer quantity) {
+        this.itemId = itemId;
+        this.memberId = memberId;
+        this.quantity = quantity;
+    }
 }
--- src/main/java/com/mysite/rmss/service/order/OrderService.java
+++ src/main/java/com/mysite/rmss/service/order/OrderService.java
@@ -1,5 +1,6 @@
 package com.mysite.rmss.service.order;
 
+import com.mysite.rmss.dto.order.OrderRequestDto;
 import com.mysite.rmss.repository.order.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,11 +15,15 @@ public class OrderService {
 
     @Transactional
     public Long order () {
+        OrderRequestDto orderRequestDto = new OrderRequestDto(1L, 1L, 2);
+
         // 필요: 고객정보 / 쇼핑몰 정보 / 주문 내역서
         // TODO: memberId 로 Member 불러옴
         // TODO: shop 정보 불러옴
         // TODO: 받은 주문 내역서(dto)로 배송정보 생성
-        // TODO: 위
+        // TODO: 위 내용으로 orderItem 을 생성하고
+        // TODO: 생성한 orderItem 으로 order 를 생성함
+        // TODO: repository 로 저장하면 끝
         return 1L;
     }
 }
