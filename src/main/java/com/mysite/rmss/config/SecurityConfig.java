e274186493d78a560f0bcdfcbab463696c5d69f8
TipsyBoy <gudah13@naver.com>
2023-02-23 17:02:51 +0900

    페이지 이용중 로그인이 필요한 작업이 있는 경우 로그인 페이지로 보냈다가 다시 마지막으로 사용한 페이지로 보내야하는데, 다음에 다시 구현할 것

--- src/main/java/com/mysite/rmss/config/SecurityConfig.java
+++ src/main/java/com/mysite/rmss/config/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -36,7 +35,6 @@ public class SecurityConfig {
         httpSecurity
                     .formLogin()
                     .loginPage("/members/login")
-//                    .successHandler(new SavedRequestAwareAuthenticationSuccessHandler())
                     .defaultSuccessUrl("/")
                 .and()
                     .logout()
