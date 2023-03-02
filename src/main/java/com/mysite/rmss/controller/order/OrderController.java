83d61d2cec12cea12536ac12533a65f441d729c0
TipsyBoy <gudah13@naver.com>
2023-03-02 15:45:00 +0900

    주문 -> 장바구니 로 선회중인데 머리아프네

--- src/main/java/com/mysite/rmss/controller/order/OrderController.java
+++ src/main/java/com/mysite/rmss/controller/order/OrderController.java
@@ -2,11 +2,14 @@ package com.mysite.rmss.controller.order;
 
 import com.mysite.rmss.config.auth.CurrentMember;
 import com.mysite.rmss.domain.member.Member;
+import com.mysite.rmss.dto.item.ItemResponseDto;
 import com.mysite.rmss.dto.order.OrderRequestDto;
+import com.mysite.rmss.service.item.ItemService;
 import com.mysite.rmss.service.order.OrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,24 +22,40 @@ import javax.validation.Valid;
 public class OrderController {
 
     private final OrderService orderService;
+    private final ItemService itemService;
 
     @GetMapping("/order/orderSheet")
-    public String viewOrderSheet(@Valid @ModelAttribute OrderRequestDto orderRequestDto,
-                                 BindingResult bindingResult,
-                                 @CurrentMember Member member) {
-
+    public String viewOrderSheet(@ModelAttribute OrderRequestDto orderRequestDto,
+                                 @CurrentMember Member member,
+                                 Model model) {
         if (member == null) {
             return "redirect:/members/login";
             // TODO: 이 로그인 이후에 상품 페이지로 다시 돌아가게 하고 싶은데, 어떻게 할까
         }
 
-        if (bindingResult.hasErrors()) {
-            return "shop/shopItemDetail";
-        }
-        
-        // 주문
-        orderService.order(orderRequestDto);
+        ItemResponseDto itemResponseDto = itemService.findById(orderRequestDto.getItemId());
+        model.addAttribute("items", itemResponseDto);
 
-        return "redirect:/"; // TODO: 주문 완료 페이지로 이동해야함.
+        return "orders/orderSheet";
     }
+
+//    @PostMapping("/order/orderSheet")
+//    public String viewOrderSheet(@Valid @ModelAttribute OrderRequestDto orderRequestDto,
+//                                 BindingResult bindingResult,
+//                                 @CurrentMember Member member) {
+//
+//        if (member == null) {
+//            return "redirect:/members/login";
+//            // TODO: 이 로그인 이후에 상품 페이지로 다시 돌아가게 하고 싶은데, 어떻게 할까
+//        }
+//
+//        if (bindingResult.hasErrors()) {
+//            return "shop/shopItemDetail";
+//        }
+//
+//        // 주문
+//        orderService.order(orderRequestDto);
+//
+//        return "redirect:/"; // TODO: 주문 완료 페이지로 이동해야함.
+//    }
 }
